4f169ecf16f6e9136d1fc1ae4ebf15f36b169d83
adiya <47381588+Adiya21j@users.noreply.github.com>
2024-04-02 22:01:51 +0800

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,17 +1,99 @@
-import boto3
+# import boto3
+# from fastapi import FastAPI, File, UploadFile
+# from fastapi.responses import HTMLResponse, FileResponse
+# from fastapi.middleware.cors import CORSMiddleware
+# import requests
+
+# app = FastAPI()
+# s3 = boto3.client('s3')
+
+# # Variable to store the last uploaded file name
+# last_uploaded_file = None
+# # Variable to store the firmware version
+# firmware_version = "0.0.1"
+# # Variable to track total files downloaded
+# total_files_downloaded = 0
+
+# # HTML form for file upload and firmware manipulation
+# upload_form_html = """
+# <!DOCTYPE html>
+# <html>
+# <head>
+#     <title> File Upload Form </title>
+# </head>
+# <body>
+#     <h2>Current Firmware Version</h2>
+#     <p>{firmware_version}</p>
+#     <h2>Total Files Downloaded</h2>
+#     <p>{total_files_downloaded}</p>
+
+#     <h2>Upload firmware.bin file</h2>
+#     <form action="/upload_file/" method="post" enctype="multipart/form-data">
+#         <input type="file" name="file"><br><br>
+#         <input type="submit" value="Upload">
+#     </form>
+
+#     <h2>Download last uploaded file</h2>
+#     <form action="/get_file/" method="get">
+#         <input type="submit" value="Download Last Uploaded File">
+#     </form>
+# </body>
+# </html>
+# """
+
+# @app.get("/", response_class=HTMLResponse)
+# async def home():
+#     return upload_form_html.format(firmware_version=firmware_version, total_files_downloaded=total_files_downloaded)
+
+# @app.get("/get_version/")
+# async def get_version():
+#     return {"version": firmware_version}
+
+# @app.get("/set_version/")
+# async def set_version(version: str):
+#     global firmware_version
+#     firmware_version = version
+#     return {"message": "Firmware version set successfully", "version": firmware_version}
+
+# @app.post("/upload_file/")
+# async def upload_file(file: UploadFile = File(...)):
+#     global last_uploaded_file
+#     with open(file.filename, "wb") as buffer:
+#         buffer.write(file.file.read())
+#     last_uploaded_file = file.filename
+#     return {"filename": file.filename}
+
+# # @app.post("/upload_file/")
+# # async def create_upload_file(file: UploadFile = File(...)):
+# #     # Upload file to S3
+# #     s3.upload_fileobj(file.file, 'your-bucket-name', file.filename)
+# #     last_uploaded_file = file.filename
+# #     return {"filename": file.filename}
+
+# @app.get("/get_file/")
+# async def get_file():
+#     global last_uploaded_file, total_files_downloaded
+
+#     if last_uploaded_file:
+#         total_files_downloaded += 1
+#         return FileResponse(path=last_uploaded_file, media_type='application/octet-stream', filename=last_uploaded_file)
+#     else:
+#         total_files_downloaded += 1
+#         return FileResponse(path="blynk.bin", media_type='application/octet-stream', filename="blynk.bin")
+
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse, FileResponse
 
 app = FastAPI()
 
+# Variable to store the last uploaded file name
+last_uploaded_file = None
 # Variable to store the firmware version
 firmware_version = "0.0.1"
 # Variable to track total files downloaded
 total_files_downloaded = 0
 
-# S3 setup
-s3 = boto3.client('s3')
-bucket_name = 'your-bucket-name'
+uploaded = None
 
 # HTML form for file upload and firmware manipulation
 upload_form_html = """
@@ -95,18 +177,21 @@ async def set_version(version: str):
 
 @app.post("/upload_file/")
 async def upload_file(file: UploadFile = File(...)):
-    global total_files_downloaded
-    s3.upload_fileobj(file.file, bucket_name, file.filename)
-    total_files_downloaded += 1
+    global last_uploaded_file, uploaded
+    last_uploaded_file = None
+    with open(file.filename, "wb") as buffer:
+        buffer.write(file.file.read())
+    last_uploaded_file = file.filename
+    uploaded = 21
     return {"filename": file.filename}
 
 @app.get("/get_file/")
 async def get_file():
-    global total_files_downloaded
-    total_files_downloaded += 1
-    # Here you would return the file from S3 or any other storage location
-    # Replace this with the appropriate S3 download code
-    # For example:
-    s3.download_file(bucket_name, key, '/tmp/file_to_return')
-    return FileResponse(path="/tmp/file_to_return", media_type='application/octet-stream', filename=file.filename)
-    # return FileResponse(path="blynk.bin", media_type='application/octet-stream', filename="blynk.bin")
+    global last_uploaded_file, total_files_downloaded, uploaded
+    if uploaded:
+        total_files_downloaded += 1
+        return FileResponse(path=last_uploaded_file, media_type='application/octet-stream', filename=last_uploaded_file)
+    elif (uploaded==None):
+        total_files_downloaded += 1
+        return FileResponse(path="blynk.bin", media_type='application/octet-stream', filename="blynk.bin")
+        # return {"message": "No FIle"}
